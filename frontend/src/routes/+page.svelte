7a42e0734a6354ca34a14c822b9e395ea8d5bd56
Markus Seeli <markus@seeli.eu>
2025-03-23 17:50:51 +0100

    init

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import api from '$lib/api';
+  let status = 'loading';
+
+  onMount(async () => {
+    try {
+      const res = await api.get('/health');
+      status = res.data.status;
+    } catch {
+      status = 'error';
+    }
+  });
+</script>
+
+<h1>API Health: {status}</h1>
